5acdddf3fff1109879dcfd7377f7b88a14a3754c
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-11-15 23:25:17 +0100

    1:M Con4 do this!

--- University.Core/Entities/Enrollment.cs
+++ University.Core/Entities/Enrollment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,5 +16,12 @@ namespace University.Core.Entities
 
         //Conv 1 nullable foreign key!!
         //public Student Student { get; set; }
+
+        //Conv 3 = Conv 1 + Conv 2
+        //public Student Student { get; set; }
+
+        //Conv 4 add foreign key
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
     }
 }

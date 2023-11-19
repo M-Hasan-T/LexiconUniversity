17a4a7ead1aaf6e2632bc8edfba6ef263e35334d
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-11-19 14:45:12 +0100

    Get StudentIndexViewModel

---
+++ University.Web/Globalusing.cs
@@ -0,0 +1,9 @@
+﻿global using University.Core;
+global using University.Core.Entities;
+global using University.Persistence;
+global using University.Persistence.Data;
+global using University.Web.Models;
+global using University.Web.Models.ViewModels;
+
+
+

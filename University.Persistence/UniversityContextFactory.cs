63ffa35bf7cb3584d8abbd55177c8ff6d408a8b1
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-11-25 18:14:30 +0100

    Add UniversityContextFactory

---
+++ University.Persistence/UniversityContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Persistence.Data;
+
+namespace University.Persistence
+{
+    internal class UniversityContextFactory : IDesignTimeDbContextFactory<UniversityContext>
+    {
+        public UniversityContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<UniversityContext>();
+            options.UseSqlServer("Not used for scaffolding");
+            return new UniversityContext(options.Options);
+        }
+    }
+
+}

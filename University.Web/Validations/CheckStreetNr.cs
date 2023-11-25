64c27c0b3af3c42bb2803d0a25af32af5031d9c6
M. Hasan Tuncer <muhammedh.tuncer@gmail.com>
2023-11-26 00:51:20 +0100

    Client validation

--- University.Web/Validations/CheckStreetNr.cs
+++ University.Web/Validations/CheckStreetNr.cs
@@ -1,14 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace University.Web.Validations
 {
-    public class CheckStreetNr : ValidationAttribute
+    public class CheckStreetNr : ValidationAttribute, IClientModelValidator
     {
         private readonly int max;
 
         public CheckStreetNr(int max = 20)
         {
             this.max = max;
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            context.Attributes.Add("data-val", "true");
+            context.Attributes.Add("data-val-streetnr", "Error from JS");
+            context.Attributes.Add("data-val-streetnr-max", $"{max}");
+
         }
 
         public override bool IsValid(object? value)
